18464a82c0c30a0f2703613ac4f3922b31ef3aa5
Jayam Vimal <40145761+CrazyLavendar@users.noreply.github.com>
2021-04-11 16:09:07 +0530

    Time: 470 ms (5.09%), Space: 40.2 MB (44.30%) - LeetHub

--- matrix-block-sum/matrix-block-sum.java
+++ matrix-block-sum/matrix-block-sum.java
@@ -1,45 +1,29 @@
 class Solution {
     
-    public static int[][] dirs =  {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     int k;
     public int[][] matrixBlockSum(int[][] mat, int k) {
         
         this.k = k;
-        if(k==0)
-            return mat;
-        
-        
         int m = mat.length , n =mat[0].length ;
-        
         int[][] res = new int[m][n];
-        
-        
         for(int i =0 ; i< m ;i++){
             for(int j =0 ; j< n ;j++){
-                int[][] visited = new int[m][n];
                 res[i][j] = solve(i , j , mat);
             }
         }
-        
         return res;
     }
     
     public int solve(int row , int col , int[][] mat){
         
-         // System.out.println(row + " " + col);
-        // add all row
         int res =0 ;
         int m = mat.length , n =mat[0].length ;
         for(int i = row -k; i<= row+k ; i++){
             for(int j =col - k ; j<= col + k ;j++){
-                // System.out.println(row + " " + col + " " +i + " " + j);
                 if(i>=0 && i<m  && j>=0 && j<n)
-                res += mat[i][j];
+                    res += mat[i][j];
             }
-            
         }
-        
-   
         return res;
     }
 }

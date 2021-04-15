6df6c93d9945a7e22d112cbcceef36de819db2cd
Jayam Vimal <40145761+CrazyLavendar@users.noreply.github.com>
2021-04-15 23:13:22 +0530

    Time: 0 ms (100.00%), Space: 35.6 MB (74.08%) - LeetHub

---
+++ fibonacci-number/fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n<2)
+            return n;
+        int curr = 1 , prev = 0;
+        
+        for(int i = 2 ; i<= n ;i++){
+            int temp = curr;
+            curr += prev;
+            prev = temp;
+        }
+        return curr;
+    }
+}

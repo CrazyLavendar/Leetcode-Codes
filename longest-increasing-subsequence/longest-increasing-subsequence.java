341a7e958d4b16f1e216622d83c389dbe4495202
Vimal Kumar <vkcs20120@student.nitw.ac.in>
2021-05-28 12:20:50 +0530

    Time: 2 ms (98.88%), Space: 38.7 MB (44.31%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,28 +1,58 @@
 public class Solution {
 
-    public int lengthOfLIS(int[] nums) {
-        // return lengthofLIS(nums, Integer.MIN_VALUE, 0);
+    
+    //N^2
+//     public int lengthOfLIS(int[] nums) {
+//         // return lengthofLIS(nums, Integer.MIN_VALUE, 0);
         
-        int len = nums.length;
-        int[] dp = new int[len];
-        int res = 1;
-        for(int i = 0 ;i< len ; i++){
+//         int len = nums.length;
+//         int[] dp = new int[len];
+//         int res = 1;
+//         for(int i = 0 ;i< len ; i++){
             
-            dp[i] = 1;
-            for(int j = 0 ; j<i ; j++){
+//             dp[i] = 1;
+//             for(int j = 0 ; j<i ; j++){
                 
-                if(nums[j] < nums[i]){
+//                 if(nums[j] < nums[i]){
                     
-                    dp[i] = Math.max(dp[i] , 1 + dp[j]);
-                }
+//                     dp[i] = Math.max(dp[i] , 1 + dp[j]);
+//                 }
                 
                 
-            }
-            res = Math.max(res , dp[i]);
+//             }
+//             res = Math.max(res , dp[i]);
             
-        }
+//         }
         
-        return res;
+//         return res;
+    
+    
+    
+    //nlogn
+    
+    public int lengthOfLIS(int[] nums) {
+    int[] tails = new int[nums.length];
+    int size = 0;
+    for (int x : nums) {
+        int i = 0, j = size;
+        while (i != j) {
+            int m = (i + j) / 2;
+            if (tails[m] < x)
+                i = m + 1;
+            else
+                j = m;
+        }
+        tails[i] = x;
+        if (i == size) ++size;
+
+
+//         for(int xx : tails)
+//             System.out.print(xx + " , ");
+// System.out.println("");
+    }
+    return size;
+
+// Runtime: 2 ms
         
     }
 

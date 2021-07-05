0bb6a05ea06de27bc561f4a27eb5a23266f7dd19
Vimal Kumar <vkcs20120@student.nitw.ac.in>
2021-07-05 21:20:03 +0530

    Time: 11 ms (45.39%), Space: 41.3 MB (83.79%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.java
+++ top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,25 +1,63 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        List<Integer> res = new ArrayList<>();
+//         List<Integer> res = new ArrayList<>();
+//         Map<Integer , Integer> map = new HashMap<>();
+//         for(int x : nums){
+//             if(!map.containsKey(x))
+//                 map.put(x , 0);
+//             map.put(x , map.get(x) + 1);
+//         }
+        
+//         List<List<Integer>> list = new ArrayList<>();
+        
+//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//         list.add(new ArrayList<>(List.of(entry.getKey() , entry.getValue())));
+// }
+
+//         Collections.sort(list , (a , b) -> b.get(1) - a.get(1));
+        
+//             for(int i = 0 ; i< k ; i++)
+//                 res.add(list.get(i).get(0));
+        
+//         return res.stream().mapToInt(i -> i).toArray();
+        
+        
         Map<Integer , Integer> map = new HashMap<>();
+        
         for(int x : nums){
-            if(!map.containsKey(x))
-                map.put(x , 0);
-            map.put(x , map.get(x) + 1);
+         if(!map.containsKey(x))
+            map.put(x,0);
+            
+        map.put(x , (map.get(x) + 1));
+        }
+
+        List<Integer>[] bucket = new List[nums.length +1];
+        
+        for(Integer key : map.keySet()){
+            int freq = map.get(key);
+            if(bucket[freq] == null)
+                bucket[freq] = new ArrayList<>();
+            bucket[freq].add(key);
         }
         
-        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
         
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-    // System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-        list.add(new ArrayList<>(List.of(entry.getKey() , entry.getValue())));
-}
+        for(int i =nums.length ; i >=0 ; i--){
+            
+            
+            if(bucket[i] != null){
+                for(int x : bucket[i])
+                    res.add(x);
+            }
+            
+            if(res.size() >= k)
+                break;
+            
+        }
+        
+        
 
-        Collections.sort(list , (a , b) -> b.get(1) - a.get(1));
-        
-            for(int i = 0 ; i< k ; i++)
-                res.add(list.get(i).get(0));
-        
         return res.stream().mapToInt(i -> i).toArray();
+
     }
 }

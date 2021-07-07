e5e3ee78befc4d46f079d8c65633ff099ab77f85
Vimal Kumar <vkcs20120@student.nitw.ac.in>
2021-07-07 17:13:03 +0530

    Time: 12 ms (77.97%), Space: 38.4 MB (69.69%) - LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,18 +1,17 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         
+        /*
         int[] dp = new int[amount +1];
         
         Arrays.fill(dp , 10001);
         
-        
-        for(int i = 0 ; i< amount +1 ; i++){
+        dp[0] =0;
+        for(int i = 1 ; i< amount +1 ; i++){
             
             for(int j =0 ; j<coins.length; j++){
                 
-                if(i==0)
-                    dp[i] =0; // if amount is zero no coins required
-                else if(i >= coins[j])
+                 if(i >= coins[j])
                     dp[i] = Math.min(dp[i] , dp[i-coins[j]] + 1);
                 
             }
@@ -20,5 +19,27 @@ class Solution {
         }
         
         return dp[amount] ==  10001 ? -1 : dp[amount];
+        */
+
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp , Integer.MAX_VALUE - 1);
+        dp[0] = 0;
+        
+        
+        for(int i = 1 ; i<= amount ; i++){
+            for(int j = 0; j<coins.length ; j++)
+                
+                if( i >= coins[j]){
+                   dp[i] = Math.min(dp[i-coins[j]] + 1, dp[i]); 
+                }
+            }
+        
+//         for(int x : dp)
+//             System.out.print(x + " ");
+        
+        return dp[amount] == Integer.MAX_VALUE - 1? -1 : dp[amount] ;
+        
     }
+    
+    
 }

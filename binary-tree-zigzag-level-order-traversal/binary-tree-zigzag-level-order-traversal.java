084e1fa54682930fc16e1aec437b5e90eb7b6592
Vimal Kumar <vkcs20120@student.nitw.ac.in>
2021-07-13 18:57:06 +0530

    Time: 2 ms (13.70%), Space: 39.8 MB (6.85%) - LeetHub

--- binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -14,25 +14,47 @@
  * }
  */
 class Solution {
-    int maxHeight = -1;
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+     
+         List<List<Integer>> res = new ArrayList<>();
+        helper(root , res ,0);
         
-        List<List<Integer>> res = new ArrayList<>();
-        helper(root , res , 0);
+        for(int i = 1; i<res.size() ; i++)
+            if(i%2 == 1)
+                reverse(res.get(i));
+        
         return res;
     }
-        public void helper(TreeNode root, List<List<Integer>> res , int h){
-        if(root == null)
-            return;
-        if(h > maxHeight){
-            res.add(new ArrayList<Integer>());
-            maxHeight++;
-        }
-        if(h%2 == 0)
-            res.get(h).add(root.val);
-        else
-            res.get(h).add(0,root.val);
-        helper(root.left ,res , h+1);
-        helper(root.right, res, h+1);
+    
+    public void helper(TreeNode root ,  List<List<Integer>> res , int pos){
+        
+        if(root == null) return;
+        
+        if(pos == res.size())
+            res.add(new ArrayList<>());
+        res.get(pos).add(root.val);
+        
+        helper(root.left , res , pos+1);
+        helper(root.right , res , pos+1);
+        
     }
+    
+    public void reverse(List<Integer> list){
+        int left = 0 , right = list.size() - 1;
+        
+        while(left < right){
+            int temp = list.get(left);
+            list.set(left++ , list.get(right));
+            list.set(right-- , temp);
+            
+        }
+    }
+    
+//     public int height(TreeNode root){
+//         if(root == null)
+//             return 0;
+//         return Math.max(height(root.left) ,height(root.right)) + 1;
+//     }
+    
+    
 }

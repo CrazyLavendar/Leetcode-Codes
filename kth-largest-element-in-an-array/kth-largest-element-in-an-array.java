d525a748d60e79aeba20c21d1418e9563f5fc6aa
Vimal Kumar <vkcs20120@student.nitw.ac.in>
2021-08-02 16:24:57 +0530

    Time: 8 ms (28.21%), Space: 39.1 MB (75.68%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -2,49 +2,58 @@ class Solution {
     public int findKthLargest(int[] nums, int k) {
         
         
-        // System.out.println(pivotPartition(nums , 0 , nums.length - 1));
         
-        // for(int x : nums)
-        //     System.out.print(x + " ");
+        // System.out.println(pivot(nums , 0 , nums.length -1));
         
-        
-        
-        int index = nums.length - k ;
-        int start = 0 , end = nums.length -1;        
-        
-         while (start < end) {
-            int pivot = pivotPartition(nums, start, end);
-            if (pivot < index) start = pivot + 1; 
-            else if (pivot > index) end = pivot - 1;
-            else return nums[pivot];
-        }
-        
-        return nums[start];
-        
-    }
-    
-    public int pivotPartition(int[] nums , int start , int end){
-        int pivot = start; // Choose Pivot
-        int i = start +1 ,j = i;
-        
-        for( ; j<= end ; j++){
+        int left = 0 , right = nums.length -1 , n = nums.length - k;
+        while(left  <= right){
             
-            if(nums[j] < nums[pivot]){
+            int pivot = pivot(nums , left , right);
+        // System.out.println(pivot);
+            
+            if(pivot == n)
+                return nums[pivot];
+            else if(pivot < n){
+                left = pivot +1;
                 
-                swap(nums, i++ , j);
-                
+            }else{
+                right = pivot -1;
             }
             
+            
         }
+    
+        return 0;
         
-        swap(nums , pivot , i-1);
         
-        return i-1;
     }
     
+    public int pivot(int[] nums , int left , int right){
+        
+        int select = nums[left];
+        int i = left+1 , j = left +1;
+        while( i <= right ){
+         if(nums[i] <= select){
+             swap(nums , i , j);
+             j++;
+         }
+         i++;    
+        }
+       // nums[j] = select;
+        swap(nums , left , j-1);
+//         for(int x : nums)
+//             System.out.print(x + " ");
+        
+//         System.out.println("");
+        return j-1;
+        
+    }
     public void swap(int[] nums , int i , int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+        
     }
+    
+    
 }

f7b1659e1611108b545c0221153c57c7c2c3eb53
Vimal Kumar <vkcs20120@student.nitw.ac.in>
2021-08-03 09:52:44 +0530

    Time: 11 ms (94.90%), Space: 38.4 MB (64.93%) - LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,45 +1,27 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        
-        /*
-        int[] dp = new int[amount +1];
-        
+    public int coinChange(int[] coins, int a) {
+     
+        int n = coins.length;
+        int[] dp = new int[a+1];
         Arrays.fill(dp , 10001);
-        
-        dp[0] =0;
-        for(int i = 1 ; i< amount +1 ; i++){
-            
-            for(int j =0 ; j<coins.length; j++){
-                
-                 if(i >= coins[j])
-                    dp[i] = Math.min(dp[i] , dp[i-coins[j]] + 1);
-                
-            }
-            
-        }
-        
-        return dp[amount] ==  10001 ? -1 : dp[amount];
-        */
-
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp , Integer.MAX_VALUE - 1);
         dp[0] = 0;
-        
-        
-        for(int i = 1 ; i<= amount ; i++){
-            for(int j = 0; j<coins.length ; j++)
+        for(int x : coins){
+            
+            for(int i = 0 ; i<= a ; i++){
                 
-                if( i >= coins[j]){
-                   dp[i] = Math.min(dp[i-coins[j]] + 1, dp[i]); 
+               if(x<= i){
+                   
+                    dp[i] = Math.min(dp[i] , dp[i-x] + 1);
+                   // System.out.println(i + " " + x + " " + dp[i]);
                 }
             }
+        }
         
-//         for(int x : dp)
-//             System.out.print(x + " ");
+        // for(int x : dp){
+        //     System.out.print(x + " ");
+        // }
         
-        return dp[amount] == Integer.MAX_VALUE - 1? -1 : dp[amount] ;
-        
+       
+        return dp[a]==10001? -1 : dp[a];
     }
-    
-    
 }
